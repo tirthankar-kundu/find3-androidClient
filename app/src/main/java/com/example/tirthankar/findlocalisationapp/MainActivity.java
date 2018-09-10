6a2abae241a787487d36799abe666b886b9effdf
tirthankar_kundu <tirthankar_kundu@persistent.com>
2018-09-10 23:48:39 +0530

    Find android Client

---
+++ app/src/main/java/com/example/tirthankar/findlocalisationapp/MainActivity.java
@@ -0,0 +1,240 @@
+package com.example.tirthankar.findlocalisationapp;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText eTfamily,eTdevice,eTserver,eTlocation;
+    private TextView textViewRes;
+    private String family,device,server,location;
+    private boolean tracking;
+    private CheckBox checkTracking;
+    private Button btn;
+    private Intent serviceIntent;
+    private boolean error = false;
+    private final int ACCESS_COARSE_LOCATION =1;
+    private boolean appAllowed = true;
+
+    //Volley Request queue
+    RequestQueue requestQueue;
+
+    StringRequest stringRequest;
+    private Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        askPermission();
+
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        serviceIntent = new Intent(this, ScanService.class);
+
+        textViewRes = findViewById(R.id.textViewRes);
+        eTfamily = findViewById(R.id.editTextFamily);
+        eTdevice = findViewById(R.id.editTextDevice);
+        eTserver = findViewById(R.id.editTextFamilyServer);
+        eTlocation = findViewById(R.id.editTextLocation);
+        checkTracking = findViewById(R.id.checkBox);
+        checkTracking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkTracking.isChecked()){
+                    disableEditText(eTdevice);
+                    disableEditText(eTfamily);
+                    disableEditText(eTlocation);
+                    tracking = true;
+                }
+                else{
+                    enableEditText(eTdevice);
+                    enableEditText(eTfamily);
+                    enableEditText(eTlocation);
+                    tracking = false;
+                }
+            }
+        });
+
+        btn = findViewById(R.id.btnStart);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                family = eTfamily.getText().toString();
+                if(family.isEmpty()){
+                    eTfamily.setError("Family name cannot be empty");
+                    eTfamily.setFocusable(true);
+                    error = true;
+                }else if(usedNames(family)){
+                    eTfamily.setError("Use other family name");
+                    eTfamily.setFocusable(true);
+                    error = true;
+                }
+
+                device = eTdevice.getText().toString();
+                if(device.isEmpty()){
+                    eTdevice.setError("Device name cannot be empty");
+                    eTdevice.setFocusable(true);
+                    error = true;
+                }
+
+                server = eTserver.getText().toString();
+                if(server.isEmpty()){
+                    eTserver.setError("Server address cannot be empty");
+                    eTserver.setFocusable(true);
+                    error = true;
+                }
+
+                location = eTlocation.getText().toString();
+                if(location.isEmpty() && !tracking){
+                    eTlocation.setError("Location cannot be empty");
+                    eTlocation.setFocusable(true);
+                    error = true;
+                }
+
+                if(!error && appAllowed) {
+                    if(tracking){
+                        stringRequest = new StringRequest(server+"/api/v1/location/"+family+"/"+device, new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                                    JSONObject jsonObject = new JSONObject(response);
+                                    JSONObject analysis = jsonObject.getJSONObject("analysis");
+                                    JSONArray guesses = analysis.getJSONArray("guesses");
+                                    JSONObject guess = guesses.getJSONObject(0);
+                                    textViewRes.setText("You are at location "+guess.getString("location")+" with probability of "+guess.getString("probability"));
+                                    Log.d("VolleyRes",guess.toString());
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d("Volley",error.getLocalizedMessage());
+                            }
+                        });
+                    }
+                    if (btn.getText().toString().toLowerCase().equals("start scan")) {
+                        btn.setText("STOP SCAN");
+                        serviceIntent.putExtra("family",family);
+                        serviceIntent.putExtra("device",device);
+                        serviceIntent.putExtra("location",location);
+                        serviceIntent.putExtra("server",server);
+                        serviceIntent.putExtra("tracking",tracking);
+                        startService(serviceIntent);
+
+                        if(tracking){
+                            startTracking();
+                        }
+
+                    } else {
+                        btn.setText("START SCAN");
+                        stopService(serviceIntent);
+                        if(tracking)
+                            handler.removeCallbacks(startTracking);
+                    }
+                }else{
+                    Toast.makeText(MainActivity.this, "Permission not granted", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        //start a service based on button click
+    }
+
+    private void askPermission() {
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, ACCESS_COARSE_LOCATION);
+            return;
+        }else{
+            // Write you code here if permission already given.
+        }
+    }
+
+    Runnable startTracking = new Runnable() {
+        @Override
+        public void run() {
+
+            requestQueue.add(stringRequest);
+            //Call this code after every 10 seconds to perform realtime tracking
+            handler.postDelayed(startTracking,10000);
+        }
+    };
+
+    private void startTracking() {
+        handler=new Handler();
+        handler.post(startTracking);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == ACCESS_COARSE_LOCATION){
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                appAllowed = true;
+            } else {
+                // permission denied, boo! Disable the
+                // functionality that depends on this permission.
+                appAllowed = false;
+            }
+        }
+    }
+
+    private boolean usedNames(String family) {
+        List<String> alreadyUsedNames = new ArrayList<>();
+        alreadyUsedNames.add("ibm_v1");
+        alreadyUsedNames.add("testdb");
+        alreadyUsedNames.add("testdb1");
+        alreadyUsedNames.add("testdb2");
+        alreadyUsedNames.add("testdb3");
+        alreadyUsedNames.add("testdb4");
+        alreadyUsedNames.add("testdb5");
+        if(alreadyUsedNames.contains(family))
+            return true;
+        return  false;
+    }
+
+    private void disableEditText(EditText editText) {
+        //editText.setFocusable(false);
+        editText.setEnabled(false);
+        editText.setCursorVisible(false);
+    }
+    private void enableEditText(EditText editText) {
+        //editText.setFocusable(true);
+        editText.setEnabled(true);
+        editText.setCursorVisible(true);
+    }
+}
---
+++ app/src/main/java/com/example/tirthankar/findlocalisationapp/ScanService.java
@@ -0,0 +1,281 @@
+package com.example.tirthankar.findlocalisationapp;
+
+import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class ScanService extends Service{
+    // logging
+    private final String TAG = "ScanService";
+
+    int mStartMode;       // indicates how to behave if the service is killed
+    IBinder mBinder;      // interface for clients that bind
+    boolean mAllowRebind; // indicates whether onRebind should be used
+
+    boolean isScanning = false;
+    private final Object lock = new Object();
+
+    // wifi scanning
+    private WifiManager wifi;
+
+    // bluetooth scanning
+    private BluetoothAdapter BTAdapter = BluetoothAdapter.getDefaultAdapter();
+    BluetoothBroadcastReceiver receiver = null;
+
+    // post data request queue
+    RequestQueue queue;
+    private JSONObject jsonBody = new JSONObject();
+    private JSONObject bluetoothResults = new JSONObject();
+    private JSONObject wifiResults = new JSONObject();
+
+    private String familyName;
+    private String locationName;
+    private String deviceName;
+    private String serverAddress = "";
+    private boolean allowGPS = false;
+    private boolean tracking = false;
+    @Override
+    public void onCreate() {
+
+        // The service is being created
+        Log.d(TAG, "creating new scan service");
+        queue = Volley.newRequestQueue(this);
+        // setup wifi
+        wifi = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+        if (wifi.isWifiEnabled() == false) {
+            wifi.setWifiEnabled(true);
+        }
+        // register wifi intent filter
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        registerReceiver(mWifiScanReceiver, intentFilter);
+
+        try {
+            // setup bluetooth
+            Log.d(TAG, "setting up bluetooth");
+            if (receiver == null) {
+                receiver = new BluetoothBroadcastReceiver();
+                registerReceiver(receiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+            }
+        } catch (Exception e) {
+            Log.e(TAG, e.toString());
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+
+        familyName = intent.getStringExtra("family");
+        deviceName = intent.getStringExtra("device");
+        locationName = intent.getStringExtra("location");
+        serverAddress = intent.getStringExtra("server");
+        tracking = intent.getBooleanExtra("tracking",false);
+
+
+        Log.d(TAG, "familyName: " + familyName +" server "+serverAddress);
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                Log.d(TAG,"scheduler");
+                synchronized (lock) {
+                    if (isScanning == false) {
+                        doScan();
+                    }
+                }
+            }
+        }, 1000, 10000);
+
+        return START_STICKY;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        // All clients have unbound with unbindService()
+        return mAllowRebind;
+    }
+
+    @Override
+    public void onDestroy() {
+        // The service is no longer used and is being destroyed
+        Log.v(TAG, "onDestroy");
+        try {
+            if (receiver != null)
+                unregisterReceiver(receiver);
+        } catch (Exception e) {
+            Log.w(TAG, e.toString());
+        }
+        try {
+            if (mWifiScanReceiver != null)
+                unregisterReceiver(mWifiScanReceiver);
+        } catch (Exception e) {
+            Log.w(TAG, e.toString());
+        }
+        stopSelf();
+        super.onDestroy();
+    }
+
+    // bluetooth reciever
+    private class BluetoothBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String name = device.getAddress().toLowerCase();
+                Log.v(TAG, "bluetooth: " + name + " => " + rssi + "dBm");
+                try {
+                    bluetoothResults.put(name, rssi);
+                } catch (Exception e) {
+                    Log.e(TAG, e.toString());
+                }
+            }
+        }
+    }
+
+    private void doScan() {
+        synchronized (lock) {
+            if (isScanning == true) {
+                return;
+            }
+            isScanning = true;
+        }
+        bluetoothResults = new JSONObject();
+        wifiResults = new JSONObject();
+        BTAdapter.startDiscovery();
+        if (wifi.startScan()) {
+            Log.d(TAG, "started wifi scan");
+        } else {
+            Log.w(TAG, "started wifi scan false?");
+        }
+        Log.d(TAG, "started discovery");
+    }
+
+
+    private final BroadcastReceiver mWifiScanReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context c, Intent intent) {
+            // This condition is not necessary if you listen to only one action
+            if (intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
+                Log.d(TAG, "timer off, trying to send data");
+                List<ScanResult> wifiScanList = wifi.getScanResults();
+                for (int i = 0; i < wifiScanList.size(); i++) {
+                    String name = wifiScanList.get(i).BSSID.toLowerCase();
+                    int rssi = wifiScanList.get(i).level;
+                    Log.d(TAG, "wifi: " + name + " => " + rssi + "dBm");
+                    try {
+                        wifiResults.put(name, rssi);
+                    } catch (Exception e) {
+                        Log.e(TAG, e.toString());
+                    }
+                }
+                sendData();
+                BTAdapter.cancelDiscovery();
+                BTAdapter = BluetoothAdapter.getDefaultAdapter();
+                synchronized (lock) {
+                    isScanning = false;
+                }
+            }
+        }
+    };
+
+    public void sendData() {
+        try {
+            String URL = serverAddress + "/data";
+            Log.d(TAG,"url:"+URL);
+            jsonBody.put("f", familyName);
+            jsonBody.put("d", deviceName);
+            if(!tracking)
+                jsonBody.put("l", locationName);
+            else
+                jsonBody.put("l", "");
+            jsonBody.put("t", System.currentTimeMillis());
+            JSONObject sensors = new JSONObject();
+            sensors.put("bluetooth", bluetoothResults);
+            sensors.put("wifi", wifiResults);
+            jsonBody.put("s", sensors);
+
+
+            final String mRequestBody = jsonBody.toString();
+            Log.d(TAG, mRequestBody);
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.d(TAG, "response"+response);
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e(TAG, error.toString());
+                }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return mRequestBody == null ? null : mRequestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", mRequestBody, "utf-8");
+                        return null;
+                    }
+                }
+
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    if (response != null) {
+                        responseString = new String(response.data);
+                    }
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                }
+            };
+
+            queue.add(stringRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
